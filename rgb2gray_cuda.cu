8424daa0d23e78c475327357a5289c7f5482f0ad
nassimb0u <in_boulechfar@esi.dz>
2022-01-31 14:08:03 +0100

    Feat cuda implementation

--- rgb2gray_cuda.cu
+++ rgb2gray_cuda.cu
@@ -9,6 +9,16 @@
 #include "stb_image/stb_image_write.h"
 
 #define MAX_PATH 255
+#define BLOCK_WIDTH 32
+#define BLOCK_HEIGHT 32
+
+__global__ void rgba_to_grayscale(uint8_t *d_rgba_image, uint8_t *d_gray_image, int image_width, int image_height) {
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int index = y * image_width + x;
+    if (y > image_height || x > image_width) return;
+    d_gray_image[index] = (uint8_t)(.299f * d_rgba_image[index * 3] + .587f * d_rgba_image[index * 3 + 1] + .114f * d_rgba_image[index * 3 + 2]);
+}
 
 const char *get_file_ext(char *file_path) {
     const char *p, *dot = file_path;
@@ -53,15 +63,25 @@ int main(int argc, char **argv) {
         printf("Unable to allocate memory for the output image\n");
         exit(1);
     }
-    clock_t begin = clock();
-    for (uint8_t *p = input_img, *pg = output_img; p != input_img + img_size; p += channels, pg += gray_channels) {
-        *pg = (uint8_t)(.299f * *p + .587f * *(p + 1) + .114f * *(p + 2));
-        if (channels == 4 && gray_channels == 2)
-            *(pg + 1) = *(p + 3);
-    }
-    clock_t end = clock();
-    // time is milliseconds
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC * 1000;
+    uint8_t *d_input_img, d_output_img;
+    cudaEvent_t start, stop;
+    float elapsed_time;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    size_t input_img_size = width * height * channels;
+    cudaMalloc((void **)&d_input_img, input_img_size);
+    cudaMalloc((void **)&d_output_img, output_img_size);
+    cudaMemcpy(d_input_img, input_img, input_img_size, cudaMemcpyHostToDevice);
+    const dim3 block_size(BLOCK_WIDTH, BLOCK_HEIGHT, 1);
+    unsigned int nb_blocksx = (unsigned int)(width / BLOCK_WIDTH + 1);
+    unsigned int nb_blocksy = (unsigned int)(height / BLOCK_HEIGHT + 1);
+    const dim3 grid_size(nb_blocksx, nb_blocksy, 1);
+    cudaEventRecord(start, 0);
+    rgba_to_grayscale<<<grid_size, block_size>>>(d_input_img, d_output_img, width, height);
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsed_time, start, stop);
+    cudaMemcpy(output_img, d_output_img, output_img_size, cudaMemcpyDeviceToHost);
     const char *output_file_extension = get_file_ext(output_file);
     if (!(strcmp(output_file_extension, "jpg") || strcmp(output_file_extension, "jpeg") || strcmp(output_file_extension, "JPG") || strcmp(output_file_extension, "JPEG")))
         stbi_write_jpg(output_file, width, height, gray_channels, output_img, 100);
@@ -71,6 +91,10 @@ int main(int argc, char **argv) {
         stbi_write_png(output_file, width, height, gray_channels, output_img, width * gray_channels);
     stbi_image_free(input_img);
     stbi_image_free(output_img);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaFree(d_a);
+    cudaFree(d_a);
     printf("check '%s' (took %fms)\n", output_file, time_spent);
     return 0;
 }
